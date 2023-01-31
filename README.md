74ca332d7e634873b7e4c42b50b799ed458d79c6
natcracker <117683132+web3cryptoidea@users.noreply.github.com>
2023-01-31 13:22:51 +0000

    added color change to js

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # work-day-scheduler
+This repository contains a simple calendar application that allows a user to save events for each hour of the day by modifying starter code.
+
+## Description
+
+This application displays the current day at the top of the calender when a user opens the planner, present timeblocks for standard business hours when the user scrolls down. Each timeblock is color-coded based on past, present, and future when the timeblock is viewed. Clicking on a timeblock a user can enter an event and save it by clicking on save button of a timeblock.
+This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript.  
+
+## Installation
+N/A
+
+## Usage 
+This webpage is deployed  at live URL, you can access it using: https://web3cryptoidea.github.io/work-day-scheduler/
+
+It looks like this:
+
+ ![Start quiz](assets/images/start-quiz.png)
+
+ ![Highscores](assets/images/highscores.png)
+
+ ![Example of question](assets/images/questions-example.png)
+
+ ![End of test sreen](assets/images/end-screen.png)
+ 
+
+
+ 
+## Credits
+
+- Moment JS Library [Moment.js](https://momentjs.com/) 
+- jQuery, a fast and feature-rich JavaScript library [jQuery] (https://jquery.com/)
+
+
+## License
+Please refer to the LICENSE in the repo.
+
+ 
+ 
+
+  
--- starter/assets/script.js
+++ starter/assets/script.js
@@ -1,60 +1,53 @@
- 
-
 var currentHour = new Date().getHours();
+var timeDisplay = $('#currentDay');
 
-// Get all time entries
-var entries = document.querySelectorAll(".time");
 
-$(".saveBtn").on("click", function(){
+  //Display current day
+  function displayDay() {
+    var rightNow = moment().format('ddd, MMM Do YYYY');
+    timeDisplay.text(rightNow);
+  }
+
+  displayDay();
+
+$(document).ready(function() {
+ 
+  //Store the entry data in the local storage
+  $(".saveBtn").on("click", function(){
     var inputArea = $(this).siblings(".errand-name").val();
     console.log(inputArea);
     var rowHour = $(this).parent().attr("id");
     localStorage.setItem(rowHour, inputArea);
 })
+ //Retrieve the entry data from local storage    
+  $("#8 .errand-name").val(localStorage.getItem("8"));
+  for (var i=8; i < 18; i++) {
+  $("#" + i + " .errand-name").val(localStorage.getItem("" + i));
+  }
 
-$("#8 .errand-name").val(localStorage.getItem("8"));
-for (var i=8;  i < 18; i++)  {
-$(`#${i} .errand-name`).val(localStorage.getItem(`${i}`));
-}
+// Loop through the time blocks and set a different color
+// depending on whether its past, future or present hour
+  $('.row').each(function() {
+    var entry = $(this).find('.time').text();
+    var entryHour = parseInt(entry.split(' ')[0]);
+    var entryPeriod = entry.split(' ')[1];
 
-
-// Loop through each entry
-function entryLoop() {
-for (var i = 0; i < entries.length; i++) {
-    var entry = entries[i];
-    var entryText = entry.innerText;
-    var entryHour = parseInt(entryText.split(" ")[0]);
-    var entryPeriod = entryText.split(" ")[1];
-
-    // Compare current hour and entry hour
     if (entryPeriod == "pm") {
-        entryHour += 12;
+      if (entryHour == 12) {
+        entryHour == 12
+       } else {
+      entryHour += 12;
+      }
     }
     if (currentHour < entryHour) {
-        entry.parentNode.style.backgroundColor = "red";
+      $(this).find('.errand-name').addClass("future");
+    } else if (currentHour == entryHour) {
+      $(this).find('.errand-name').addClass("present");
     } else {
-        entry.parentNode.style.backgroundColor = "gray";
+      $(this).find('.errand-name').addClass("past");
     }
-}
-}
-entryLoop();
+    $(this).find('.saveBtn').addClass("saveBtn");
+  });
+});
+ 
 
-// Get all diary arrends entries
-var diaryEntry = document.querySelectorAll(".errand-name");
-function saveEntry() {
-    // Get the value of the diary entry text 
-    for (var i = 0; i < diaryEntry.length; i++) {
-        var dentry = diaryEntry[i].value;
-          // Save the entry to local storage
-    localStorage.setItem("diaryEntry", dentry);
-     // On page load, check if there is a saved diary entry in local storage
-  // If so, populate the diary entry textarea with the saved entry
-    }  
-  }
-
-  window.onload = function() {
-    var savedEntry = localStorage.getItem("diaryEntry");
-    if (savedEntry) {
-      document.querySelectorAll(".errand-name").value = savedEntry;
-    }    
-  }
